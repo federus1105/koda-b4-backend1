819485aab91a6edab0ac2a89c64a9de8ce8b9eb5
federus1105 <federusrudi@gmail.com>
2025-11-05 15:13:25 +0700

    Feat ( main ) : funcion using middleware cors

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"backend-day1/middleware"
 	"backend-day1/view"
 	"fmt"
 )
 
 func main() {
 	router := view.SetupRouter()
+	router.Use(middleware.CORSMiddleware)
 	fmt.Println("Server running")
 	router.Run("localhost:8080")
 }
