20aefd51cb046e66807765f958212298748a2dea
federus1105 <federusrudi@gmail.com>
2025-11-06 00:47:26 +0700

    feat : function swagger documentation

--- view/Routes.go
+++ view/Routes.go
@@ -2,11 +2,18 @@ package view
 
 import (
 	"backend-day1/controllers"
+	"backend-day1/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	user := router.Group("/users")
 	{
