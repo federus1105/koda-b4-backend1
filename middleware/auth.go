215deef229e9222a643543c8ed1c4fcbcd57e7c8
federus1105 <federusrudi@gmail.com>
2025-11-06 16:58:22 +0700

    Feat ( middleware ) : function verify token

--- middleware/auth.go
+++ middleware/auth.go
@@ -34,3 +34,26 @@ func (c *Claims) GenToken() (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
 	return token.SignedString([]byte(jwtSecret))
 }
+
+func (c *Claims) VerifyToken(token string) error {
+	godotenv.Load()
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return errors.New("no secret found")
+	}
+	parsedToken, err := jwt.ParseWithClaims(token, c, func(t *jwt.Token) (any, error) { return []byte(jwtSecret), nil })
+	if err != nil {
+		return err
+	}
+	if !parsedToken.Valid {
+		return jwt.ErrTokenExpired
+	}
+	iss, err := parsedToken.Claims.GetIssuer()
+	if err != nil {
+		return err
+	}
+	if iss != os.Getenv("JWT_ISSUER") {
+		return jwt.ErrTokenInvalidIssuer
+	}
+	return nil
+}
